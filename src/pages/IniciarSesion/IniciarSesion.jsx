67b72c8a10578060f0f20f30021bc31f86d92fbc
Mauricio <m.mauricio@correo.unimet.edu.ve>
2024-03-17 14:44:30 -0400

    Pgina de Inicio de Sesion

--- src/pages/IniciarSesion/IniciarSesion.jsx
+++ src/pages/IniciarSesion/IniciarSesion.jsx
@@ -1,18 +1,55 @@
 import React from 'react'
-
+import styles from "./IniciarSesion.module.css"
+import { Link, useNavigate } from "react-router-dom";
 export  function IniciarSesion() {
   return (
     
-    <div>
-        <div>Iniciar Sesion</div>
-        <div>Iniciar Sesion</div>
-        <div>Iniciar Sesion</div>
-        <div>Iniciar Sesion</div>
-        <div>Iniciar Sesion</div>
-        <div>Iniciar Sesion</div>
-        <div>Iniciar Sesion</div>
-        <div>Iniciar Sesion</div>
-        <div>Iniciar Sesion</div>
+    <div className={styles.containerGlob}>
+      <div className={styles.container}>
+        <form className={styles.form} >
+          <h1 className={styles.title}>¡Bienvenido de vuelta!</h1>
+          
+
+          <div className={styles.inputContainer}>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Correo electrónico"
+            
+          />
+          </div>
+
+          <div className={styles.inputContainer}>
+            <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Contraseña"
+            
+          />
+         </div>
+         <div className={styles.linedivision}>
+          <hr  /> 
+         </div>
+
+         <div className={styles.loginAltern}>
+          <button><img src="./src/assets/google.png" alt="" /></button>
+           <button><img src="./src/assets/facebook.png" alt="" /></button>
+         </div>
+
+        <button type="submit" className={styles.submitBtn}>
+          Iniciar Sesión
+        </button>
+
+        <Link className={styles.loginRedirect}>
+          ¿No tienes una cuenta?{" "}
+          <span className={styles.redirectLink}>Regístrate</span>
+        </Link>
+      </form>
+      </div>
+      <img src="./src/assets/imgIniciarSesion.png" alt="" />
+
     </div>
   )
 }
