ec60a8e9017f84e745a49466e539af130ca1a41b
davdev <david.davila@correo.unimet.edu.ve>
2024-03-18 14:51:14 -0400

    feat: :sparkles: exports on firebase.js

--- src/data/firebase/index.js
+++ src/data/firebase/index.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage"
+import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,6 +24,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
+const googleProvider = new auth.GoogleAuthProvider();
 
 
-export { analytics, db };
+export { analytics, db, storage, auth, googleProvider};
