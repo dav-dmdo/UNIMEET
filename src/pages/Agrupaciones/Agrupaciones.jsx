eb145b8cd0742b74520e3067d05e6a70430f7556
GabrielDBrito <britogabrielc@gmail.com>
2024-03-24 15:04:19 -0400

    layout msi agrupaciones

--- src/pages/Agrupaciones/Agrupaciones.jsx
+++ src/pages/Agrupaciones/Agrupaciones.jsx
@@ -1,8 +1,17 @@
-
 import React from 'react'
+import styles from "./Agrupaciones.module.css";
+
 
 export function Agrupaciones() {
   return (
-    <div>Agrupaciones</div>
+      <main>  
+          <section className={styles.middlebox}>
+          <h1 className={styles.title}>¡Hola Nombre de Usuario!</h1>
+          <h2 className={styles.description}>Tus agrupaciones actuales son:</h2>              
+          </section>
+          
+          
+      </main>
+    
   )
 }
---
+++ src/pages/ListadoAgrupaciones/ListadoAgrupaciones.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from "./ListadoAgrupaciones.module.css";
+
+
+export function ListadoAgrupaciones() {
+  return (
+      <main>  
+          <section className={styles.middlebox}>
+          <h1 className={styles.title}>Listado de Agrupaciones</h1>          
+          </section>
+          
+          
+      </main>
+    
+  )
+}
