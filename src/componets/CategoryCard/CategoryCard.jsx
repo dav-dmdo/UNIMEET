0887ec31437bfcdd20cfc99cc0c1c6ed6a24ca4b
Andrea Linares <linares.andrea@correo.unimet.edu.ve>
2024-03-24 18:27:20 -0400

    :heart: Fixed responsive GUI Categorias

--- src/componets/CategoryCard/CategoryCard.jsx
+++ src/componets/CategoryCard/CategoryCard.jsx
@@ -1,21 +1,29 @@
 import styles from './CategoryCard.module.css';
 
 export default function CategoryCard(props) {
-    const {category, groups, image} =props;
+  const { category, groups, image } = props;
   return (
     <div className={styles.Layout}>
-{/*TODO - SEE HOW CAN YOU MAKE THIS CHOOSE THE IMAGE BY THE CATEGORY*/}
-        <div className={styles.images}>
-        <img className={styles.image} src={props.image} alt="" />
-        </div>
-        <div className={styles.info}>
+      {/* TODO - SEE HOW CAN YOU MAKE THIS CHOOSE THE IMAGE BY THE CATEGORY */}
+      <div className={styles.images}>
+        <img className={styles.image} src={image} alt="" />
+      </div>
+      <div className={styles.info}>
         <div className={styles.title}>
-        <h1>{props.category}</h1>
+          <h1>{category}</h1>
         </div>
         <div className={styles.text}>
-           <p>{props.groups}</p>
+          {Array.isArray(groups) ? (
+            groups.map((group, index) => (
+              <p key={index} style={{ marginBottom: '10px' }}>
+                {group}
+              </p>
+            ))
+          ) : (
+            groups
+          )}
         </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/pages/Categorias/Categorias.jsx
+++ src/pages/Categorias/Categorias.jsx
@@ -1,28 +1,27 @@
 import CategoryCard from '../../componets/CategoryCard/CategoryCard';
+import useCategories from '../../controllers/Hooks/useCategories';
 import styles from './Categorias.module.css';
 
 export  function Categorias() {
+  const categorias =useCategories();
+
+
+  if (!categorias) {
+    return <p>Cargando...</p>;
+    
+  }
+  console.log(categorias[0].agrupaciones);
   return (
     <div className={styles.mainBox}>
-      {/*ANCHOR - STEM*/}
-      <div className={styles.category}>
-      <CategoryCard category= "STEM" groups="Fórmula SAE" image= "./src/assets/STEM.png"/>
+    {categorias.map((categoria, index) => (
+      <div className={styles.category} key={index}>
+        <CategoryCard
+          category={categoria.nombre.toUpperCase()}
+          groups={categoria.agrupaciones}
+          image={categoria.imagen}
+        />
       </div>
-
-      {/*ANCHOR - DEBATE*/}
-      <div className={styles.category}>
-      <CategoryCard category= "DEBATE" groups="Fórmula SAE" image= "./src/assets/DEBATE.png"/>
-      </div>
-
-      {/*ANCHOR - CULTURAL*/}
-      <div className={styles.category}>
-      <CategoryCard category= "STEM" groups="Fórmula SAE" image= "./src/assets/STEM.png"/>
-      </div>
-
-      {/*ANCHOR - OTHER*/}
-      <div className={styles.category}>
-      <CategoryCard category= "STEM" groups="Fórmula SAE" image= "./src/assets/STEM.png"/>
-      </div>
-      </div>
+    ))}
+  </div>
   )
 }
