b994815f0e3f18ec76700de1fe4ee40898867320
anabellaj <j.anabella@correo.unimet.edu.ve>
2024-03-29 13:14:06 -0400

    💩 intentando arreglar los colores que se cambian solos (?)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -41,17 +41,17 @@ export function Home() {
         <div className={styles.preguntas}>
           <div className={styles.block1}>
             <div className={styles.preg1}><h2>¿Cómo puedo unirme a una agrupación?</h2>
-              <p>¡Es muy sencillo! Sólo debes iniciar sesión y luego hacer click en el botón de Unirme dentro de la agrupación deseada.</p>
+              <p className={styles.answer}>¡Es muy sencillo! Sólo debes iniciar sesión y luego hacer click en el botón de Unirme dentro de la agrupación deseada.</p>
             </div>
             <div className={styles.preg2}><h2>¿Debo ser estudiante de la UNIMET para unirme a las agrupaciones?</h2>
-              <p>Si, las agrupaciones están dirigidas únicamente hacia estudiantes actuales de la Universidad Metropolitana.</p>
+              <p className={styles.answer}>Si, las agrupaciones están dirigidas únicamente hacia estudiantes actuales de la Universidad Metropolitana.</p>
             </div></div>
             <div className={styles.block2}>
             <div className={styles.preg3}><h2>¿De qué sirve mi aporte monetario?</h2>
-              <p>El aporte monetario se realiza al unirte a una agrupación y el mismo es opcional. Este beneficia grandemente a la agrupación ya que permite cubrir gastos lo cual facilita la creación de nuevas iniciativas.</p>
+              <p className={styles.answer}>El aporte monetario se realiza al unirte a una agrupación y el mismo es opcional. Este beneficia grandemente a la agrupación ya que permite cubrir gastos lo cual facilita la creación de nuevas iniciativas.</p>
             </div>
             <div className={styles.preg4}><h2>¿Qué beneficio me da pertenecer a una agrupación?</h2>
-              <p>Pertencer a una agrupación estudiantil tiene numerosos beneficios, como conocer a personas con intereses similares lo que te permite integrarte en una comunidad con objetivos similares y apoyo mutuo.</p>
+              <p className={styles.answer}>Pertencer a una agrupación estudiantil tiene numerosos beneficios, como conocer a personas con intereses similares lo que te permite integrarte en una comunidad con objetivos similares y apoyo mutuo.</p>
             </div></div>
           </div>
       </div>
