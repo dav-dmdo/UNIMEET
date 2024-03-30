1cd185750e97dbb336bff256dc2fc238e9dae8ef
davdev <david.davila@correo.unimet.edu.ve>
2024-03-30 18:33:05 -0400

    fix: :bug: using UserContext in navbar component

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,13 +1,11 @@
  import { createContext, useState } from "react";
  import { useUser } from "../hooks/useUser";
  import { auth } from "../data/firebase";
- import { signInWithEmailAndPassword } from "firebase/auth";
 
  export const UserContext = createContext();
 
  export const UserProvider = ({ children }) => {
    const { user, userIsLoading } = useUser();
-   signInWithEmailAndPassword(auth, "david@gmail.com", 1234567890);
 
    const auxGroup = {
      categoria: "Otras",
