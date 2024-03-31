d7340d9ea2280792b14bc687a70f1a2fa8720448
davdev <david.davila@correo.unimet.edu.ve>
2024-03-31 00:44:57 -0400

    se muestra cuando no hay integrantes

--- src/components/Group/GroupFeedbackMembers/MembersList/MembersList.jsx
+++ src/components/Group/GroupFeedbackMembers/MembersList/MembersList.jsx
@@ -2,11 +2,26 @@ import MembersItem from './MembersItem/MembersItem'
 import styles from './MembersList.module.css'
 
 const MembersList = ({members}) => {
+  if (!members) {
+    return (
+      <MembersItem
+        name={"No hay integrantes todavia..."}
+        photoURL={"https://via.placeholder.com/150"}
+        num={"--"}
+      />
+    );
+  }
   return (
     <div className={styles.container}>
-        {members.map((member, index) => (
+        {members.length > 0 ? (members.map((member, index) => (
             <MembersItem key={index} {...member} num={index+1}/>
-        ))}
+        ))): (
+            <MembersItem
+            name={"--"}
+            email={"--"}
+            num={"0"}
+          />
+        )}
 
     </div>
   )
