07479d03179fa46e218f9ae06024aac440e39f66
davdev <david.davila@correo.unimet.edu.ve>
2024-03-31 00:39:03 -0400

    Arregaldo el import

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,28 +1,12 @@
  import { createContext, useState } from "react";
  import { useUser } from "../hooks/useUser";
- import { auth } from "../data/firebase";
 
  export const UserContext = createContext();
 
  export const UserProvider = ({ children }) => {
    const { user, userIsLoading } = useUser();
 
-   const auxGroup = {
-     categoria: "Otras",
-     descripcion:
-       "Asociación de Rescate y Condicionamiento Animal. ARCA Unimet es un centro terapéutico con el objetivo de reducir el número de animales abandonados.",
-     img: "https://firebasestorage.googleapis.com/v0/b/unimeet-is.appspot.com/o/ARCA%20Unimet.png?alt=media&token=81edc8d1-a5ce-4d26-b9f0-99b06f0a4392",
-     instagram: "@arcaunimet",
-     mision:
-       "ARCA Unimet es una agrupación que incentiva la adopción de mascotas, especialmente perros adultos, la esterilización de las mismas y el buen trato animal, mediante actividades de carácter educativo enfocadas en estos principios. Asimismo, la interacción con nuestros perros, tanto dentro como fuera de la sede, forman parte vital de dicho proceso. Nuestra labor va dirigida a niños y jóvenes principalmente que hagan vida dentro de la comunidad unimetana, como a instituciones educativas afines a la Universidad Metropolitana.",
-     nombre: "Arca Unimet",
-     ubicacion: "Caracas, Venezuela",
-     vision:
-       "Ser una agrupación capaz de fomentar la buena interacción entre las personas y los animales, la tenencia responsable de mascotas. Por otra parte, uno de nuestros objetivos a largo plazo es establecernos como un servicio comunitario propio enfocado en incentivar el buen trato animal mediante actividades de carácter educativo.",
-     year: "2016",
-   };
-
-   const [groupToShow, setGroupToShow] = useState(auxGroup);
+   const [groupToShow, setGroupToShow] = useState({});
 
    return (
      <UserContext.Provider
