9c65ac5cad606c9c03e123b7a90c985d83828939
Mauricio <m.mauricio@correo.unimet.edu.ve>
2024-03-31 22:48:57 -0400

    url hosting

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,10 +1,10 @@
 import styles from "./Search.module.css"
-
+import search from "../../assets/search.png"
 export function Search({onChange}) {
 
   return (
     <div className={styles.background}>
-        <img src="./src/assets/search.png" height='30px' className={styles.image}/>
+        <img src={search} height='30px' className={styles.image}/>
         <input placeholder="Haz click para buscar una agrupación específica" className={styles.inputs} onChange={onChange}>
             
         </input>
