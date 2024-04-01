6547dd50d0e14369194dabc1722d0b651fe6d89b
aandreareyess <113395933+aandreareyess@users.noreply.github.com>
2024-03-31 20:46:59 -0400

    🔧fixing adding agrupaciones

--- src/pages/AgregarAgrupacion/AgregarAgrupacion.jsx
+++ src/pages/AgregarAgrupacion/AgregarAgrupacion.jsx
@@ -2,51 +2,113 @@ import styles from './AgregarAgrupacion.module.css';
 import { Selector } from '../../componets/Selector/Selector';
 import useCategories from '../../controllers/Hooks/useCategories';
 import { useState } from 'react';
+import { collection,  addDoc, query,getDocs,where} from "firebase/firestore"
 import { db } from '../../data/firebase';
 
 
+
 export default function AgregarAgrupacion(){
+
+    const [nombre, setNombre] = useState('');
+    const [mision, setMision] = useState('');
+    const [vision, setVision] = useState('');
+    const [instagram, setInstagram] = useState('');
+    const [email, setEmail ]= useState('');
+
     
     const [selectedCategoria, setSelectedCategoria] = useState('');
     const categorias = useCategories();
+
+
+
     const [formData, setFormData] = useState({
     nombre: '',
     agrupaciones: [],
   });
 
-  const handleCategoriaSelected = (event) => {
-    setSelectedCategoria(event.target.value);
-  };
 
-    const handleOnChange = (event) => {
-        const { name, value } = event.target;
-        setFormData({
-          ...formData,
-            [name]: value,
-    });
-  };
+  const handleCategory = (event) => {
+    const { name, value } = event.target;
+    setSelectedCategoria({
+      ...selectedCategoria,
+        [name]: value,
+});
+};
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+
+
+const handleNombre = (e) => {
+  setNombre(e.target.value);
+};
+
+const handleMision = (e) => {
+  setMision(e.target.value);
+};
+
+const handleVision = (e) => {
+  setVision(e.target.value);
+};
+
+const handleInstagram = (e) => {
+  setInstagram(e.target.value);
+};
+
+const handleEmail = (e) => {
+  setEmail(e.target.value);
+};
+
+
+// Función para verificar si un usuario ya existe
+const verificarAgrupacion = async (agrupacionNueva) => {
+  try {
+    const verificar = query(collection(db, 'agrupacion'), where('nombre', '==', agrupacionNueva));
+    const querySnapshot = await getDocs(verificar);
+    return !querySnapshot.empty;
+  } catch (error) {
+    return false; // Devolvemos falseen caso de que no se encuentre
+  }
+};
+
+  const onClick = async () => {
+
+    if (
+      verificarAgrupacion!= false
+    ){
+
+    alert('el nombre de la agrupacion ya existe cree otro');
+    return;
+  }
+
+    // Creamos un objeto con los datos de la nueva agrupacion 
+    const nuevaAgrupacion= {
+      nombre: nombre,
+      mision: mision,
+      vision:vision,
+      instagram:instagram,
+      email: email,
+      categorias: selectedCategoria,
+    };
 
     try {
-      await db.collection('agrupaciones').add({
-        nombre: formData.nombre,
-        mision : formData.mision,
-        vision: formData.vision,
-        instagram: formData.instagram,
-        email: formData.email,
-        categorias: formData.categorias
-      });
 
-      console.log('Datos del formulario enviados a Firebase');
+      await addDoc(collection(db, 'agrupaciones'), nuevaAgrupacion);
+      console.log('Agrupacion añadida con exito');
+
+      // Limpiamos los campos del formulario después de agregar el usuario
+      setNombre('');
+      setMision('');
+      setVision('');
+      setInstagram('');
+      setEmail('');
+
     } catch (error) {
-      console.error('Error al enviar datos a Firebase:', error);
+      console.error('Error al añadir la agrupacion', error);
     }
   };
 
 
     return(
+      
         <div className={styles.cuerpo}>
          <h1 className={styles.titulo}>Agregar Agrupacion</h1>
 
@@ -54,37 +116,35 @@ export default function AgregarAgrupacion(){
             <div className={styles.containerLeft}>
             <div className={styles.form}>
                 <label htmlFor='nombre' className={styles.labels}>Nombre de la Agrupacion </label>
-                <input onSubmit={handleSubmit} type='string' id='nombre'></input>
+                <input type='string' id='nombre' value={nombre} onChange={handleNombre}></input>
             </div>
             <div className={styles.form}>
                 <label htmlFor='mision' className={styles.labels}>Mision</label>
-                <input onSubmit={handleSubmit} type='string' id='mision'></input>
+                <input type='string' id='mision' value={mision} onChange={handleMision}></input>
             </div>
             <div className={styles.form}>
                 <label htmlFor='vision' className={styles.labels}>Vision</label>
-                <input onSubmit={handleSubmit} type='string' id='vision'></input>
+                <input type='string' id='vision' value={vision} onChange={handleVision}></input>
             </div>
             </div>
             <div className={styles.containerRight}>
             <div className={styles.form}>
                 <label htmlFor='instagram' className={styles.labels}>Instagram</label>
-                <input onSubmit={handleSubmit} type='string' id='instagram'></input>
+                <input  type='string' id='instagram' value={instagram} onChange={handleInstagram}></input>
             </div>
             <div className={styles.form}>
                 <label htmlFor='email' className={styles.labels}>Email</label>
-                <input onSubmit={handleSubmit} type='string' id='email'></input>
+                <input  type='string' id='email' value={email} onChange={handleEmail}></input>
             </div>
             <div className={styles.form}>
             <Selector 
-                onSubmit={handleSubmit}
                 className={styles.bigSelect}
                 placeholder='Seleccionar Categoria'
-                onChange={handleCategoriaSelected}
                 label="Seleccionar Categoria"
                 options={categorias}
                 value={selectedCategoria}
                 changeValue={(event) => {
-                  handleOnChange({
+                  handleCategory({
                     target: { name: 'nombre', value: event.target.value },
                   });
                 }}
@@ -92,7 +152,7 @@ export default function AgregarAgrupacion(){
             </div>
             </div>   
         </div>
-        <button className={styles.submit}>Agregar</button>
+        <button className={styles.submit} onClick={onClick}>Agregar</button>
         </div>
     )
 }
