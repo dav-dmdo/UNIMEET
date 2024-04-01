774a7d55f68d92712dec5c568da940626aae9c5c
Mauricio <m.mauricio@correo.unimet.edu.ve>
2024-03-31 23:46:23 -0400

    urls

--- src/components/Group/GroupFeedbackMembers/MembersList/MembersItem/MembersItem.jsx
+++ src/components/Group/GroupFeedbackMembers/MembersList/MembersItem/MembersItem.jsx
@@ -1,10 +1,12 @@
 import styles from './MembersItem.module.css'
+import userIcon from "../../../../../assets/userIcon.png"
+
 
 const MembersItem = ({name, photoURL, num}) => {
   return (
     <div className={styles.container}>
       <p className={styles.num}>{num}</p>
-      <img className={styles.img} src= { photoURL || "src/assets/userIcon.png" } alt={name}  />
+      <img className={styles.img} src= { photoURL || userIcon } alt={name}  />
       <p className={styles.name}>{name}</p>
     </div>
   )
